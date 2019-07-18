0ddd2e61047723a26cb835abe2e83beacd43365e
meowxiik <meowxiik@gmail.com>
2019-07-18 22:37:33 +0200

    Fixed syntax errors in plugin.gd

--- plugin.gd
+++ plugin.gd
@@ -5,10 +5,10 @@ func _enter_tree():
     add_custom_type("Peripheral", "Spatial", preload("PeripheralContainer.cs"), null)
     add_custom_type("Computer", "Spatial", preload("ComputerContainer.cs"), null)
     add_custom_type("VM Bridge Manager", "Node", preload("BridgeContainer.cs"), null)
-    add_custom_Type("Terminal", "Control", preload("TerminalContainer.cs), null)
+    add_custom_type("Terminal", "Control", preload("TerminalContainer.cs"), null)
 
 func _exit_tree():
     remove_custom_type("Peripheral")
     remove_custom_type("Computer")
     remove_custom_type("Hackfest")
-    remote_custom_type("Terminal")
+    remove_custom_type("Terminal")
