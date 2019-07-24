625953c437e7ea066d76eb74d80aa2332bedc11f
meowxiik <meowxiik@gmail.com>
2019-07-24 13:02:41 +0200

    Peripheral loop created

--- Pure/Peripheral.cs
+++ Pure/Peripheral.cs
@@ -6,8 +6,31 @@ public class Peripheral
     public Computer Parent;
     public object Subject;
 
+    private StreamReader _ingoing;
+    private StreamWriter _outgoing;
+    
     public void Start(StreamReader ingoing, StreamWriter outgoing)
     {
-        
+        streamRead(ingoing);
+    }
+    
+    private void streamRead(object o)
+    {
+        var reader = (StreamReader) o;
+        while (reader != null && !reader.EndOfStream)
+        {
+            var i = reader.Read();
+            OnInput(i);
+        }
+    }
+
+    public void OnInput(int i)
+    {
+        switch (Subject)
+        {
+            case null:
+                _outgoing.Write(i);
+                break;
+        }
     }
 }
